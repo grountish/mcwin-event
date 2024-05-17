280df6f0e6457fae4098bf1c25122cadabf02804
Sofia Santa Maria <yolaresofia@gmail.com>
2024-05-17 13:46:27 +0200

    update button component to include downArrow

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,14 @@
 "use client";
 import React from "react";
+import DownArrow from "./DownArrow";
 
-const Button = ({ title, classes }) => {
+const Button = ({ title, classes, hasIcon }) => {
   return (
     <div
-      className={`lg:px-20 px-7 lg:py-5 py-3 w-max transition duration-300 rounded-full font-poppinsExtraBold lg:text-xl text-base cursor-pointer hover:drop-shadow-md z-50 text-white hover:text-deepBlue hover:border-deepBlue bg-transparent border-white border ${classes}`}
+      className={`flex items-center lg:px-20 px-7 lg:py-5 py-3 w-max transition duration-300 rounded-full font-poppinsExtraBold lg:text-xl text-base cursor-pointer hover:drop-shadow-md z-50 text-white hover:text-deepBlue hover:border-deepBlue bg-transparent border-white border ${classes}`}
     >
       {title}
+      {hasIcon && <DownArrow />}
     </div>
   );
 };
